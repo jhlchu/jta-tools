0136ff1ce8969ae720217f88ea64a43f1882ef6c
jhlchu <jhlchu@gmail.com>
2020-05-27 23:11:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const path = require('path');
 
 const ignore = ['.git', 'node_modules'];
 
+if (process.argv.length === 2) {
+  console.error('Expected at least one argument!');
+  process.exit(1);
+}
+
+const projectID = process.argv[2];
+const token = process.argv[3];
+
 const folders = readdirSync(__dirname, { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
   .map(dirent => dirent.name)
   .filter(dir => !ignore.includes(dir));
 
 console.log('folders', folders);
-
-if (process.argv.length === 2) {
-  console.error('Expected at least one argument!');
-  process.exit(1);
-}
-
-console.log('project-id', process.argv[2]);
-
-console.log('pid', $PID);
+console.log('project-id', projectID);
+console.log('token', token);
